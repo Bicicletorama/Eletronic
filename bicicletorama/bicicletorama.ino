a22335e74d4730ff2a3b1683930c5899bd82efe5
Harrison <h@harrison.com.br>
2013-12-08 23:23:56 -0200

    update protocol model and send only hard data

--- bicicletorama/bicicletorama.ino
+++ bicicletorama/bicicletorama.ino
@@ -1,14 +1,8 @@
 
 #define TOTAL_PLAYERS 4
 
-
-int const MIN_ANALOG_VALUE = 0;
-int const MAX_ANALOG_VALUE = 1024;
-
-int digitalPins[] = { 2, 4, 12, 13 };
-int analogPins[] = { A0, A5, A2, A3 };
-
-int lastDigitalRead[] = { 0, 0, 0, 0 };
+int digitalPins[] = { 2, 3, 4, 5 };
+int analogPins[] = { A0, A1, A2, A3 };
 
 
 void setup()
@@ -23,23 +17,12 @@ void setup()
 void loop()
 {
   for (int i = 0; i < TOTAL_PLAYERS; i++) {
-    int value = digitalRead(digitalPins[i]);
-    if (lastDigitalRead[i] == 0 && value == 1) {
-      Serial.print(1);
-    } else {
-      Serial.print(0);
+    if (i != 0) {
+      Serial.print(" ");
     }
+    Serial.print( digitalRead(digitalPins[i]) );
     Serial.print(" ");
-    
-    lastDigitalRead[i] = value;
+    Serial.print( analogRead(analogPins[i]) );
   }
-  
-  for (int i = 0; i < TOTAL_PLAYERS; i++) {
-    Serial.print( map(analogRead(analogPins[i]), MIN_ANALOG_VALUE, MAX_ANALOG_VALUE, -100, 100)/100.f );
-    Serial.print(" ");
-  }
-  
   Serial.println();
-  
-  delay(2);
 }
