810e62bad75fc9646e73595e3985cc7d5a675ae9
Harrison <h@harrison.com.br>
2013-12-08 23:03:40 -0200

    isa version

--- bicicletorama/bicicletorama.ino
+++ bicicletorama/bicicletorama.ino
@@ -2,12 +2,11 @@
 #define TOTAL_PLAYERS 4
 
 
-int const MIN_ANALOG_VALUE = 200;
-int const MAX_ANALOG_VALUE = 800;
+int const MIN_ANALOG_VALUE = 0;
+int const MAX_ANALOG_VALUE = 1024;
 
-int digitalPins[] = { 2, 4, 5, 6 };
-int analogPins[] = { A2, A4, A1, A3 };
-
+int digitalPins[] = { 2, 4, 12, 13 };
+int analogPins[] = { A0, A5, A2, A3 };
 
 int lastDigitalRead[] = { 0, 0, 0, 0 };
 
@@ -15,7 +14,7 @@ int lastDigitalRead[] = { 0, 0, 0, 0 };
 void setup()
 {
   Serial.begin(9600);
-  
+ 
   for (int i = 0; i < TOTAL_PLAYERS; i++) {
     pinMode(digitalPins[i], INPUT);
   }
@@ -35,15 +34,8 @@ void loop()
     lastDigitalRead[i] = value;
   }
   
-  float direction = 0;
   for (int i = 0; i < TOTAL_PLAYERS; i++) {
-    direction = map(analogRead(analogPins[i]), MIN_ANALOG_VALUE, MAX_ANALOG_VALUE, -100, 100)/100.f;
-    if (direction > 1.0f) {
-      direction = 1.0f;
-    } else if (direction < -1.0f) {
-      direction = -1.0f;
-    }
-    Serial.print(direction);
+    Serial.print( map(analogRead(analogPins[i]), MIN_ANALOG_VALUE, MAX_ANALOG_VALUE, -100, 100)/100.f );
     Serial.print(" ");
   }
   
