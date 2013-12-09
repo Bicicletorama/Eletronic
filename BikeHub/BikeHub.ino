b4f8573a76f1b17bc97103ac393851f252f2319f
Harrison <h@harrison.com.br>
2013-12-09 00:55:48 -0200

    add connection pin

--- BikeHub/BikeHub.ino
+++ BikeHub/BikeHub.ino
@@ -1,6 +1,7 @@
 
 #define TOTAL_PLAYERS 4
 
+int connectionPins[] = { 2, 4, 6, 8 };
 int wheelPins[] = { 3, 5, 7, 9 };
 int handlebarPins[] = { A0, A1, A2, A3 };
 
@@ -10,6 +11,7 @@ void setup()
   Serial.begin(9600);
  
   for (int i = 0; i < TOTAL_PLAYERS; i++) {
+    pinMode(connectionPins[i], INPUT);
     pinMode(wheelPins[i], INPUT);
   }
 }
@@ -20,6 +22,8 @@ void loop()
     if (i != 0) {
       Serial.print(" ");
     }
+    Serial.print( digitalRead(connectionPins[i]) );
+    Serial.print(" ");
     Serial.print( digitalRead(wheelPins[i]) );
     Serial.print(" ");
     Serial.print( analogRead(handlebarPins[i]) );
